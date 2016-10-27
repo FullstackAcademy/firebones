ed74e1b20de5768a793d33b5dfea670d7b062c0e
Ashi Krishnan <me@ashi.works>
2016-10-27 17:35:27 -0400

    Basic express skeleton

---
+++ server/start.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const {resolve} = require('path')
+const package = require('../package.json')
+
+if (process.env.NODE_ENV !== 'production') {
+  // Logging middleware (dev & testing only)
+  app.use(require('volleyball'))
+}  
+
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  
+  // Serve static files from ../public
+  .use(express.static(resolve(__dirname, '..', 'public')))
+
+if (module === require.main) {
+  // Start listening only if we're the main module.
+  // 
+  // https://nodejs.org/api/modules.html#modules_accessing_the_main_module
+  const server = app.listen(
+    process.env.PORT || 1337,
+    () => {
+      console.log(`--- Started HTTP Server for ${package.name} ---`)      
+      console.log(`Listening on ${JSON.stringify(server.address())}`)
+    }
+  )
+}
+
+module.exports = app
+
