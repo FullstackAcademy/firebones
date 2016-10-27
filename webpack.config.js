eabccc5fd9933689f793c934b60b2b9f8070a138
Ashi Krishnan <me@ashi.works>
2016-10-27 16:39:15 -0400

    Building to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './browser/main.js',
   output: {
     path: __dirname,
-    filename: './build-output/bundle.js'
+    filename: './public/bundle.js'
   },
   context: __dirname,
   devtool: 'source-map',
