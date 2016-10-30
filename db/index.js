76cbebff807f94b3554922655ef0b2c077f2af40
Ashi Krishnan <me@ashi.works>
2016-10-29 23:55:33 -0400

    User model has a secure password.

--- db/index.js
+++ db/index.js
@@ -12,17 +12,23 @@ console.log(chalk.yellow(`Opening database connection to ${url}`));
 // create the database instance
 const db = module.exports = new Sequelize(url, {
   logging: debug, // export DEBUG=sql in the environment to get SQL queries 
-  native: true    // lets Sequelize know we can use pg-native for ~30% more speed
+  native: true,   // lets Sequelize know we can use pg-native for ~30% more speed
+  define: {
+    underscored: true,       // use snake_case rather than camelCase column names
+    freezeTableName: true,   // don't change table names from the one specified
+    timestamps: true,        // automatically include timestamp columns
+  }
 });
 
 // pull in our models
 require('./models')
 
 // sync the db, creating it if necessary
-function sync() {
-  db.sync()
+function sync(opts) {
+  db.sync(opts)
     .then(ok => console.log(`Synced models to db ${url}`))
     .catch(fail => {
+      console.error(fail)
       if (process.env.NODE_ENV === 'production') {
         console.error(fail)
         return // Don't do this auto-create nonsense in prod
@@ -34,7 +40,7 @@ function sync() {
           if (err) {
             return console.error(err)
           }
-          sync()
+          sync({force: true})
         })
     })
 }
--- db/models/user.js
+++ db/models/user.js
@@ -1,7 +1,53 @@
-'use strict';
-var Sequelize = require('sequelize')
-var db = require('APP/db')
+'use strict'
+
+const bcrypt = require('bcrypt')
+const Sequelize = require('sequelize')
+const db = require('APP/db')
 
 module.exports = db.define('users', {
-  name: Sequelize.STRING,
+  name: Sequelize.STRING,  
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+			isEmail: true,
+			notEmpty: true,
+		}
+  },
+
+  // We support oauth, so users may or may not have passwords.
+  password_digest: Sequelize.STRING,
+	password: Sequelize.VIRTUAL,
+	password_confirmation: Sequelize.VIRTUAL,
+}, {
+	indexes: [{fields: ['email'], unique: true,}],
+	instanceMethods: {
+		authenticate: function(plaintext) {
+      return new Promise((resolve, reject) =>
+        bcrypt.compare(plaintext, this.password_digest,
+          (err, result) =>
+            err ? reject(err) : resolve(result))
+      )
+    }
+	},
+  hooks: {
+    beforeCreate: setEmailAndPassword,
+    beforeUpdate: setEmailAndPassword,
+  }
 })
+
+function setEmailAndPassword(user) {
+  user.email = user.email.toLowerCase()
+  if (!user.password) return Promise.resolve(user)
+
+	if (user.password != user.password_confirmation) {
+		throw new Error("Password confirmation doesn't match password")
+	}
+
+  return new Promise((resolve, reject) =>
+	  bcrypt.hash(user.get('password'), 10, (err, hash) => {
+		  if (err) reject(err)
+		  user.set('password_digest', hash)
+      resolve(user)
+	  })
+  )
+}
