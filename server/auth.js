3c0ef8ec9ee66223c14ea5c57ab93f8f3a021984
Ashi Krishnan <me@ashi.works>
2016-10-30 14:35:55 -0400

    Debugging around passport-local.

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,5 @@
 const debug = require('debug')('auth')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 
 const User = require('APP/db/models/user')
 
@@ -27,17 +26,22 @@ passport.deserializeUser(
   }
 )
 
-passport.use(new LocalStrategy(
+passport.use(new (require('passport-local').Strategy) (
   (email, password, done) => {
+    debug('will authenticate user(email: "%s")', email)
     User.findOne({where: {email}})
       .then(user => {
         if (!user) {
+          debug('authenticate user(email: "%s") did fail: no such user', email)
           return done(null, false, { message: 'Login incorrect' })
         }
         return user.authenticate(password)
           .then(ok => {
-            if (!ok)
+            if (!ok) {
+              debug('authenticate user(email: "%s") did fail: bad password')              
               return done(null, false, { message: 'Login incorrect' })
+            }
+            debug('authenticate user(email: "%s") did ok: user.id=%d', user.id)
             done(null, user)              
           })
       })
