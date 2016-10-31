977e12262271e959457872c6b0f9c43ac8b63a30
Ashi Krishnan <me@ashi.works>
2016-10-31 14:07:41 -0400

    Hide epilogue away in users and show how to use it alongside custom routes.

--- server/api.js
+++ server/api.js
@@ -1,48 +1,12 @@
 'use strict'
-const epilogue = require('epilogue')
 
 const db = require('APP/db')
-const api = require('express').Router()
+const api = module.exports = require('express').Router()
 
 api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
-
-// Epilogue can make routes for us
-epilogue.initialize({app: api, sequelize: db})
-
-var users = epilogue.resource({
-  model: db.model('users'),
-  endpoints: ['/users', '/users/:id']
-});
-
-const mustBeLoggedIn = (req, res, context) => {
-  if (!req.user) {
-    res.status(401).send('You must be logged in')
-    return context.stop
-  }
-
-  return context.continue
-}
-
-const selfOnly = action => (req, res, context) => {
-  if (req.params.id !== req.user.id) {
-    res.status(403).send(`You can only ${action} yourself.`)
-    return context.stop
-  }
-  return context.continue  
-}
-
-const forbidden = message => (req, res, context) => {
-  res.status(403).send(message)
-  return context.stop
-}
-
-users.delete.auth(mustBeLoggedIn)
-users.delete.auth(selfOnly)
-users.list.auth(forbidden)
-users.read.auth(mustBeLoggedIn)
-
+  .use('/users', require('./users'))
 
 // Send along any errors
 api.use((err, req, res, next) => {
@@ -51,5 +15,3 @@ api.use((err, req, res, next) => {
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
-
-module.exports = api
---
+++ server/users.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const epilogue = require('./epilogue')
+const db = require('APP/db')
+
+const customUserRoutes = require('express').Router() 
+
+// Custom routes go here.
+
+module.exports = customUserRoutes
+
+// Epilogue will automatically create standard RESTful routes
+const users = epilogue.resource({
+  model: db.model('users'),
+  endpoints: ['/users', '/users/:id']
+})
+
+const {mustBeLoggedIn, selfOnly, forbidden} = epilogue.filters
+users.delete.auth(mustBeLoggedIn)
+users.delete.auth(selfOnly)
+users.list.auth(forbidden)
+users.read.auth(mustBeLoggedIn)
