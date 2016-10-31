5b8eef537eee067670a1b02c638c621cdedc5d04
Ashi Krishnan <me@ashi.works>
2016-10-31 18:04:06 -0400

    Seed script.

---
+++ db/seed.js
@@ -0,0 +1,13 @@
+const db = require('APP/db')
+
+const seedUsers = () => db.Promise.map([
+  {name: 'so many', email: 'god@example.com', password: '1234'},
+  {name: 'Barack Obama', email: 'barack@example.gov', password: '1234'},
+], user => db.model('users').create(user))
+
+db.didSync
+  .then(() => db.sync({force: true}))
+  .then(seedUsers)
+  .then(users => console.log(`Seeded ${users.length} users OK`))
+  .catch(error => console.error(error))    
+  .finally(() => db.close())
