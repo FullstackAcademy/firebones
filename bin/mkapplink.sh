21a0590b56ffecd95616f7f72a2a7e74c2a95f5b
Ashi Krishnan <me@ashi.works>
2016-10-31 12:33:13 -0400

    WIP

--- bin/mkapplink.sh
+++ bin/mkapplink.sh
@@ -1,9 +1,3 @@
-'use strict'
-
-const chalk = require('chalk')
-const fs = require('fs')
-const {join} = require('path')
-const pkg = require('./package.json')
 
 const appLink = join(__dirname, 'node_modules', 'APP')
 
@@ -47,32 +41,4 @@ function checkAppSymlink() {
   } catch (error) {
     makeAppSymlink()
   }
-}
-
-const nameError =
-`*******************************************************************
- You need to give your app a proper name.
-
- The package name
-
-    ${pkg.name}
-
-isn't valid. If you don't change it, things won't work right.
-
-Please change it in ${__dirname}/package.json
-  ~ xoxo, bones
-********************************************************************`
-
-const reasonableName = /^[a-z0-9\-_]+$/
-if (!reasonableName.test(pkg.name)) {
-  console.error(chalk.red(nameError))
-}
-
-module.exports = {
-  get name() { return pkg.name },
-  get isTesting() { return !!global.it },
-  get isProduction() {
-    return process.env.NODE_ENV === 'production'
-  },
-  package: pkg,
 }
