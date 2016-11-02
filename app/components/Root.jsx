4cff7265bad2e03ffee14f587d13a0312a822742
Ashi Krishnan <me@ashi.works>
2016-11-02 10:13:29 -0400

    React tests.

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -27,8 +27,8 @@ export default class BonesJokes extends Component {
 
     const {joke, answered} = this.state    
     return (
-      <div>
-        <h1 onClick={answered ? this.nextJoke : this.answer}>{joke.q}</h1>
+      <div onClick={answered ? this.nextJoke : this.answer}>
+        <h1>{joke.q}</h1>
         {answered && <h2>{joke.a}</h2>}
         <cite>~xoxo, bones</cite>
       </div>
---
+++ app/components/Root.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import chai, {expect} from 'chai'                                                   
+chai.use(require('chai-enzyme'))
+import {shallow} from 'enzyme'
+
+import BonesJokes from './Root'
+
+describe('<BonesJokes />', () => {
+  const joke = {
+    q: 'Why did the skeleton write tests?',
+    a: 'To see if she did anything bone-headed.',
+  }
+
+  let root
+  beforeEach('render the root', () =>
+    root = shallow(<BonesJokes />)
+  )
+
+  it('shows a joke', () => {    
+    root.setState({ joke, answered: false })
+    expect(root.find('h1')).to.have.length(1)
+    expect(root.find('h1').text()).equal(joke.q)    
+  })
+
+  it("doesn't show the answer when state.answered=false", () => {
+    root.setState({ joke, answered: false })
+    expect(root.find('h2')).to.have.length(0)
+  })
+
+  it('shows the answer when state.answered=true', () => {
+    root.setState({ joke, answered: true })
+    expect(root.find('h2')).to.have.length(1)
+    expect(root.find('h2').text()).to.equal(joke.a)
+  })
+
+  it('when tapped, sets state.answered=true', () => {
+    root.setState({ joke, answered: false })
+    root.simulate('click')
+    expect(root.state().answered).to.be.true
+  })
+})
