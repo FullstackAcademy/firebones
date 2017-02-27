5e7415e5f44b2f88f8703a31ac60ebd06a870e04
Gabriel Lebec <glebec@gmail.com>
2017-02-27 03:06:48 -0500

    feat(webpack): fast dev source maps option

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const devMode = process.env.NODE_ENV === 'development';
+
+/**
+ * Fast source maps rebuild quickly during development, but only give a link
+ * to the line where the error occurred. The stack trace will show the bundled
+ * code, not the original code. Keep this on `false` for slower builds but
+ * usable stack traces. Set to `true` if you want to speed up development.
+ */
+
+const USE_FAST_SOURCE_MAPS = false;
+
 module.exports = {
   entry: './app/main.jsx',
   output: {
@@ -7,7 +18,9 @@ module.exports = {
     filename: './public/bundle.js'
   },
   context: __dirname,
-  devtool: 'source-map',
+  devtool: devMode && USE_FAST_SOURCE_MAPS ?
+    'cheap-module-eval-source-map' :
+    'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   },
