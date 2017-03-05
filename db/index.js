1c4ba6a14b00fcdcd04bb899e86d2ef0d9f1cec0
Gabriel Lebec <glebec@gmail.com>
2017-03-05 13:14:10 -0500

    fix(tests): await proper db sync, make stateless, refactor

--- db/index.js
+++ db/index.js
@@ -1,19 +1,20 @@
 'use strict'
-const debug = require('debug')('sql')
+const app = require('APP')
+const debugSQL = require('debug')('sql') // DEBUG=sql
+const debugDB = require('debug')(`${app.name}:db`) // DEBUG=your_app_name:db
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
-const app = require('APP')
 
 const name = (process.env.DATABASE_NAME || app.name) +
   (app.isTesting ? '_test' : '')
 
 const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`
 
-console.log(chalk.yellow(`Opening database connection to ${url}`));
+debugDB(chalk.yellow(`Opening database connection to ${url}`));
 
 // create the database instance
 const db = module.exports = new Sequelize(url, {
-  logging: debug, // export DEBUG=sql in the environment to get SQL queries
+  logging: debugSQL, // export DEBUG=sql in the environment to get SQL queries
   define: {
     underscored: true,       // use snake_case rather than camelCase column names
     freezeTableName: true,   // don't change table names from the one specified
@@ -27,7 +28,7 @@ require('./models')
 // sync the db, creating it if necessary
 function sync(force=app.isTesting, retries=0, maxRetries=5) {
   return db.sync({force})
-    .then(ok => console.log(`Synced models to db ${url}`))
+    .then(ok => debugDB(`Synced models to db ${url}`))
     .catch(fail => {
       // Don't do this auto-create nonsense in prod, or
       // if we've retried too many times.
@@ -40,7 +41,7 @@ function sync(force=app.isTesting, retries=0, maxRetries=5) {
         return
       }
       // Otherwise, do this autocreate nonsense
-      console.log(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
+      debugDB(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
       return new Promise((resolve, reject) =>
         // 'child_process.exec' docs: https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
         require('child_process').exec(`createdb "${name}"`, resolve)
