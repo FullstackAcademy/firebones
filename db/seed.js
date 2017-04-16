e2b81d2b50c93a7c97f3f34db30db99bc204bff9
queerviolet <Ashi Krishnan me@ashi.works>
2017-04-16 02:17:34 -0400

    Better error handling in seed.

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const db = require('APP/db')
     , {User, Thing, Favorite, Promise} = db
+    , {mapValues} = require('lodash')
 
 const users = seed(User, {
   god: {
@@ -50,18 +53,22 @@ const favorites = seed(Favorite,
   })
 )
 
+function seedEverything() {
+  const seeded = {
+    users: users(),
+    things: things(),
+  }
+
+  seeded.favorites = favorites(seeded)
+
+  return seeded
+}
+
 if (module === require.main) {
   db.didSync
     .then(() => db.sync({force: true}))
-    .then(() => Promise.props({
-      users: users(),
-      things: things(),
-    }))
-    .then(favorites)
-    .finally(() => db.close())
+    .then(seedEverything)
 }
-
-const {mapValues} = require('lodash')
 
 class BadRow extends Error {
   constructor(key, row, error) {
@@ -72,7 +79,7 @@ class BadRow extends Error {
   }
 
   toString() {
-    return `[${this.key}] ${this.cause} while creating ${JSON.stringify(this.row, 0, 2)}\n${this.cause.stack}`
+    return `[${this.key}] ${this.cause} while creating ${JSON.stringify(this.row, 0, 2)}`
   }
 }
 
@@ -80,7 +87,9 @@ class BadRow extends Error {
 //   (others?: {...Function|Object}) -> Promise<Seeded>
 //
 // Takes a model and either an Object describing rows to insert,
-// or a function that when called, returns the same.
+// or a function that when called, returns rows to insert. returns
+// a function that will seed the DB when called and resolve with
+// a Promise of the object of all seeded rows.
 //
 // The function form can be used to initialize rows that reference
 // other models.
@@ -91,8 +100,8 @@ function seed(Model, rows) {
         mapValues(others,
           other =>
             // Is other a function? If so, call it. Otherwise, leave it alone.
-            typeof other === 'function' ? other() : other))
-        .then(rows)
+            typeof other === 'function' ? other() : other)
+      ).then(rows)
     }
 
     return Promise.resolve(rows)
