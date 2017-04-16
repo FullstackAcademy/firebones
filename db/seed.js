8805b41614c75fb63d3ace3c434b34b7685d1a91
queerviolet <Ashi Krishnan me@ashi.works>
2017-04-16 02:05:15 -0400

    Better error handling.

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,7 @@
 const db = require('APP/db')
     , {User, Thing, Favorite, Promise} = db
 
-const users = seed(User)({
+const users = seed(User, {
   god: {
     email: 'god@example.com',
     name: 'So many names',
@@ -14,61 +14,110 @@ const users = seed(User)({
   },
 })
 
-const things = seed(Thing)({
+const things = seed(Thing, {
   surfing: {name: 'surfing'},
   smiting: {name: 'smiting'},
   puppies: {name: 'puppies'},
 })
 
-const favorites = seed(Favorite)(
+const favorites = seed(Favorite,
   // We're specifying a function here, rather than just a rows object.
   // Using a function lets us receive the previously-seeded rows (the seed
   // function does this wiring for us).
   //
   // This lets us reference previously-created rows in order to create the join
-  // rows.
+  // rows. We can reference them by the names we used above (which is why we used
+  // Objects above, rather than just arrays).
   ({users, things}) => ({
     // The easiest way to seed associations seems to be to just create rows
     // in the join table.
-    'obama loves surfing': {user_id: users.barack.id, thing_id: things.surfing.id},
-    'god is into smiting': {user_id: users.god.id, thing_id: things.smiting.id},
-    'obama loves puppies': {user_id: users.barack.id, thing_id: things.puppies.id},
-    'god loves puppies': {user_id: users.god.id, thing_id: things.puppies.id},
+    'obama loves surfing': {
+      user_id: users.barack.id,
+      thing_id: things.surfing.id
+    },
+    'god is into smiting': {
+      user_id: users.god.id,
+      thing_id: things.smiting.id
+    },
+    'obama loves puppies': {
+      user_id: users.barack.id,
+      thing_id: things.puppies.id
+    },
+    'god loves puppies': {
+      user_id: users.god.id,
+      thing_id: things.puppies.id
+    },
   })
 )
 
 if (module === require.main) {
   db.didSync
     .then(() => db.sync({force: true}))
-    .then(() => ({users, things}))
+    .then(() => Promise.props({
+      users: users(),
+      things: things(),
+    }))
     .then(favorites)
     .finally(() => db.close())
 }
 
 const {mapValues} = require('lodash')
 
-// seed(Model: Sequelize.Model) -> (rows: Function|Object) -> (others?: )
-function seed(Model) {
-  return rows => others => {
+class BadRow extends Error {
+  constructor(key, row, error) {
+    super(error)
+    this.cause = error
+    this.row = row
+    this.key = key
+  }
+
+  toString() {
+    return `[${this.key}] ${this.cause} while creating ${JSON.stringify(this.row, 0, 2)}\n${this.cause.stack}`
+  }
+}
+
+// seed(Model: Sequelize.Model, rows: Function|Object) ->
+//   (others?: {...Function|Object}) -> Promise<Seeded>
+//
+// Takes a model and either an Object describing rows to insert,
+// or a function that when called, returns the same.
+//
+// The function form can be used to initialize rows that reference
+// other models.
+function seed(Model, rows) {
+  return (others={}) => {
     if (typeof rows === 'function') {
       rows = Promise.props(
         mapValues(others,
           other =>
+            // Is other a function? If so, call it. Otherwise, leave it alone.
             typeof other === 'function' ? other() : other))
         .then(rows)
     }
 
     return Promise.resolve(rows)
       .then(rows => Promise.props(
-        mapValues(rows, row =>
-          Promise.props(row)
-            .then(row => Model.create(row))
-      )))
+        Object.keys(rows)
+          .map(key => {
+            const row = rows[key]
+            return {
+              key,
+              value: Promise.props(row)
+                .then(row => Model.create(row)
+                  .catch(error => { throw new BadRow(key, row, error) })
+                )
+            }
+          }).reduce(
+            (all, one) => Object.assign({}, all, {[one.key]: one.value}),
+            {}
+          )
+        )
+      )
       .then(seeded => {
         console.log(`Seeded ${Object.keys(seeded).length} ${Model.name} OK`)
         return seeded
       }).catch(error => {
-        console.error(`Error seeding ${Model.name}`, error)
+        console.error(`Error seeding ${Model.name}: ${error} \n${error.stack}`)
       })
   }
 }
