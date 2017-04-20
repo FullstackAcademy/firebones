3c18417343030981e22311246f3c31bc36e617d9
Ben Neiswander <ben.neiswander@gmail.com>
2017-04-20 16:48:33 -0500

    names deployment example

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -5,7 +5,7 @@ deploy_remote="${DEPLOY_REMOTE:-heroku}"
 deploy_branch="${DEPLOY_BRANCH:-master}"
 
 deploy() {
-  git push -f "$deploy_remote" "$branch_name:$deploy_branch" 
+  git push -f "$deploy_remote" "$branch_name:$deploy_branch"
 }
 
 . "$(dirname $0)/build-branch.sh"
