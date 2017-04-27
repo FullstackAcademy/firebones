74519408c0dd9dc7a3d5d2c94ad57855feeb3278
queerviolet <Ashi Krishnan me@ashi.works>
2017-04-27 02:39:51 -0400

    Get rid of the blaring name error (the name doesn't really matter anymore).

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ const {resolve} = require('path')
     , pkg = require('./package.json')
     , debug = require('debug')(`${pkg.name}:boot`)
 
-    , nameError =
-`*******************************************************************
- You need to give your app a proper name.
-
- The package name
-
-    ${pkg.name}
-
-isn't valid. If you don't change it, things won't work right.
-
-Please change it in ${__dirname}/package.json
-  ~ xoxo, bones
-********************************************************************`
-
-const reasonableName = /^[a-z0-9\-_]+$/
-// RegExp.test docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
-if (!reasonableName.test(pkg.name)) {
-  console.error(chalk.red(nameError))
-}
-
 // This will load a secrets file from
 //
 //      ~/.your_app_name.env.js
