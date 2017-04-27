ab7bca01e187a6705ad8daf71438e781f94767df
queerviolet <Ashi Krishnan me@ashi.works>
2017-04-27 03:02:04 -0400

    Remove references to redux too.

--- app/main.jsx
+++ app/main.jsx
@@ -2,9 +2,7 @@
 import React from 'react'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
-import {connect, Provider} from 'react-redux'
 
-import store from './store'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
 
